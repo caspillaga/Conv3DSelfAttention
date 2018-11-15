58fe000f2a4d97c400cb50824ac58780a1466607
caspillaga <caspillaga@gmail.com>
2018-11-15 16:03:27 -0300

    fix wrong library name

--- models/trnsf_Layers.py
+++ models/trnsf_Layers.py
@@ -1,6 +1,6 @@
 ''' Define the Layers '''
 import torch.nn as nn
-from transformer.SubLayers import MultiHeadAttention, PositionwiseFeedForward
+from trnsf_SubLayers import MultiHeadAttention, PositionwiseFeedForward
 
 __author__ = "Yu-Hsiang Huang"
 
